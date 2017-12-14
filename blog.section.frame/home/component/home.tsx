f669c0d60cddee89f784bec5aaff5f7225440271
qinwen <ailsa.qin@chinaredstar.com>
2017-12-14 15:04:21 +0800

    no message

--- blog.section.frame/home/component/home.tsx
+++ blog.section.frame/home/component/home.tsx
@@ -6,55 +6,55 @@ import "../styles/style.scss";
 
 export class Home extends React.Component < IProps,
 any > {
+    banner() {
+        return (
+            <section className="intro-overlay swiper-slide">
+                <h5>Hello, World.</h5>
+                <h1>Welcome To My Blog</h1>
+                <p className="intro-position">
+                    <span>技术博客</span>
+                    <span>插件整理</span>
+                    <span>交流经验</span>
+                </p>
+                <div id="next">
+                    <a onClick={() => {this.props .goNext() }}></a>
+                </div>
+                <div className="menu">
+                    <span className="menu-item">
+                        <span className="icon-wrap">
+                            <IconComments type="blog" width={35} height={35}/>
+                        </span>
+                        <span className="tip">blog</span>
+                    </span>
+                    <span className="menu-item">
+                        <span className="icon-wrap">
+                            <IconComments type="newest" width={30} height={30}/>
+                        </span>
+                        <span className="tip">最新发布</span>
+                    </span>
+                    <span className="menu-item">
+                        <span className="icon-wrap">
+                            <IconComments type="recommend" width={30} height={30}/>
+                        </span>
+                        <span className="tip">博主推荐</span>
+                    </span>
+                    <span className="menu-item">
+                        <span className="icon-wrap">
+                            <IconComments type="contact" width ={28} height={28}/>
+                        </span>
+                        <span className="tip">联系我</span>
+                    </span>
+                </div>
+            </section>
+        )
+    }
+    
     render() {
         return (
             <div id="home-content" className="swiper-container">
                 <section className="home-bg"></section>
                 <div className="swiper-wrapper">
-                    <section className="intro-overlay swiper-slide">
-                        <h5>Hello, World.</h5>
-                        <h1>Welcome To My Blog</h1>
-                        <p className="intro-position">
-                            <span>技术博客</span>
-                            <span>插件整理</span>
-                            <span>交流经验</span>
-                        </p>
-                        <div id="next">
-                            <a
-                                onClick={() => {
-                                this
-                                    .props
-                                    .goNext()
-                            }}></a>
-                        </div>
-                        <div className="menu">
-                            <span className="menu-item">
-                                <span className="icon-wrap">
-                                    <IconComments type="blog" width={35} height={35}/>
-                                </span>
-                                <span className="tip">blog</span>
-                            </span>
-                            <span className="menu-item">
-                                <span className="icon-wrap">
-                                    <IconComments type="newest" width={30} height={30}/>
-                                </span>
-                                <span className="tip">最新发布</span>
-                            </span>
-                            <span className="menu-item">
-                                <span className="icon-wrap">
-                                    <IconComments type="recommend" width={30} height={30}/>
-                                </span>
-                                <span className="tip">博主推荐</span>
-                            </span>
-                            <span className="menu-item">
-                                <span className="icon-wrap">
-                                    <IconComments type="contact" width ={28} height={28}/>
-                                </span>
-                                <span className="tip">联系我</span>
-                            </span>
-                        </div>
-                    </section>
-
+                    {this.banner()}
                     <section id="about" className="swiper-slide">
                         <h1 className="arrow">A little<span>
                                 about
